3b333a436d6835895fe2e2f932fdc02d3bde9c6c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-16 19:17:30 +0300

    Applying Spotify Implicit Grant authentication flow upon clicking the login button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,10 +2,43 @@ import './styles.css'
 import logo from "../../assets/spotify_logo.png"
 
 const Login = () => {
+
+    const login = async () => {
+        try {
+            var client_id = process.env.REACT_APP_CLIENT_ID;
+            var redirect_uri = 'http://localhost:3000/artist-search';
+
+            var state = generateRandomString(16);
+
+            localStorage.setItem("stateKey", state);
+            var scope = 'user-read-private user-read-email';
+
+            var url = 'https://accounts.spotify.com/authorize';
+            url += '?response_type=token';
+            url += '&client_id=' + encodeURIComponent(client_id);
+            url += '&scope=' + encodeURIComponent(scope);
+            url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+            url += '&state=' + encodeURIComponent(state);
+            window.location = url
+        } catch (error) {
+            console.log("ERROR: " + error)
+        }
+    }
+
+    const generateRandomString = (length) => {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var characters_length = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * characters_length));
+        }
+       return result;
+    }
+
     return (
         <div className="container">
             <div className='row'>
-                <button className='btn'>Login 
+                <button className='btn' onClick={login}>Login 
                     <span><img className='logo' src={logo} alt='logo'/></span>
                 </button>
             </div>
