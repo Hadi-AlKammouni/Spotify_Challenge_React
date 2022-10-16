ce072698f3f8be549c60b6d63a85eb941c272b34
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-16 12:08:34 +0300

    Adding Login button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,15 @@
+import './styles.css'
+import logo from "../../assets/spotify_logo.png"
+
 const Login = () => {
     return (
-        <div>Login here!</div>
+        <div className="container">
+            <div className='row'>
+                <button className='btn'>Login 
+                    <span><img className='logo' src={logo} alt='logo'/></span>
+                </button>
+            </div>
+        </div>
     )
 }
 
