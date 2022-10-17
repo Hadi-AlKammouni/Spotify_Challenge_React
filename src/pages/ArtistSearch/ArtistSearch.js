f7719f20b9314214d7e561c2794e4e2ae4f0519a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-17 13:17:24 +0300

    Getting the query parameter and its value from the response url of the user authorization request

--- src/pages/ArtistSearch/ArtistSearch.js
+++ src/pages/ArtistSearch/ArtistSearch.js
@@ -1,4 +1,26 @@
+import { useEffect } from "react"
+
 const ArtistSearch = () => {
+    
+    // Function to separate the query parameter and its value from the response url
+    const getReturnedParams = (hash) => {
+        const string_after = hash.substring(1)
+        const params_in_url = string_after.split("&")
+        const paramsSplit = params_in_url.reduce((accumulater, current_value) => {
+            console.log(current_value)
+            const [key, value] = current_value.split("=")
+            accumulater[key] = value
+            return accumulater
+        }, {} )
+        return paramsSplit
+    }
+    
+    useEffect(() => {
+        if(window.location.hash) {
+            const object = getReturnedParams(window.location.hash)
+            console.log({ object })
+        }
+    },[])
 
     return (
         <div>Artist Search here!</div>
