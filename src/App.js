2285f87f486e269dad4d3a2995f670bf40a6b501
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-17 13:11:35 +0300

    Craeting ArtistSearch page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import ArtistSearch from "./pages/ArtistSearch/ArtistSearch";
 import Login from "./pages/Login/Login";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login/>}></Route>
+            <Route path="artist-search" element={<ArtistSearch/>}></Route>
           </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/ArtistSearch/ArtistSearch.js
@@ -0,0 +1,8 @@
+const ArtistSearch = () => {
+
+    return (
+        <div>Artist Search here!</div>
+    )
+}
+
+export default ArtistSearch
