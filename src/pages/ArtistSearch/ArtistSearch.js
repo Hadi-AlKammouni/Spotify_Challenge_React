e69f8996eef1d890d5257f0cee6e98790c31e87d
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-17 20:23:15 +0300

    Displaying artists that matches with the artist name entered as input

--- src/pages/ArtistSearch/ArtistSearch.js
+++ src/pages/ArtistSearch/ArtistSearch.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
-import logo from "../../assets/spotify_logo.png"
+
 import './styles.css'
 
 const ArtistSearch = () => {
 
-    var isArtist = false
-
     // Initialize Input State
-    const [artist, setArtist] = useState(null);
+    const [artistSearch, setArtistSearch] = useState(null);
     const [accessToken, setAccessToken] = useState("");
-    
+    const [isArtist, setIsArtist] = useState(false);
+    const [artistsData, setArtistsData] = useState(null);
+
     // Function to separate the query parameter and its value from the response url
     const getReturnedParams = (hash) => {
         const string_after = hash.substring(1)
         const params_in_url = string_after.split("&")
         const paramsSplit = params_in_url.reduce((accumulater, current_value) => {
-            console.log(current_value)
+            // console.log(current_value)
             const [key, value] = current_value.split("=")
             accumulater[key] = value
             return accumulater
@@ -26,7 +26,7 @@ const ArtistSearch = () => {
     // Calling searchForAnArtist function upon pressing the “Return” key inside the search box
     const onSearch = (e) => {
         e.preventDefault();
-        searchForAnArtist(artist)
+        searchForAnArtist(artistSearch)
     }  
     
     // Getting an artist's data 
@@ -37,66 +37,63 @@ const ArtistSearch = () => {
                 "Content-Type" : "application/json",
                 "Authorization": `Bearer ${accessToken}`
             }
-            }).then (response => response.json())
-              .then(data=> console.log(data));
+            })
+            .then (response => response.json())
+            .then(data => {
+                setIsArtist(true)
+                const artists_array = Object.entries(data)
+                setArtistsData(artists_array[0][1].items)
+            });
         } catch (error) {
           console.error(error);
         }
-      }
+    }
     
     useEffect(() => {
         if(window.location.hash) {
             const object = getReturnedParams(window.location.hash)
             setAccessToken(object.access_token)
         }
-    },[])
+    },[artistsData])
 
     return (
         <>
             {!isArtist ? 
-                null 
-                :
                 <div className="search-container">
                     <div className='search-row'>
                     <form onSubmit={onSearch}>
                         <input 
                             type="text" 
                             placeholder="Search for an artist..." 
-                            onChange={(e) => setArtist(e.target.value)}
+                            onChange={(e) => setArtistSearch(e.target.value)}
                         ></input>
                     </form>
                     </div>
                 </div>
+                :
+                null 
             }
 
-            <div className="cards-body">
-                <div className="cards-container">
-                    <div className="cards-row">
-
-                        <div className="card">
-                        <img className="img" src={logo} alt=""/>
-                            <h4>Tom</h4>
-                            <p>Followers</p>
-                            <h3>rating</h3>
+            {isArtist ? 
+                <div className="cards-body">
+                    <div className="cards-container">
+                        <div className="cards-row">
+                            {artistsData?.map((artist, i) => {
+                                console.log(artist)
+                                return (
+                                    <div className="card" key={i}>
+                                        <img className="img" src={artist.images[1]?.url} alt="artist-image"/>
+                                        <h4>{artist.name}</h4>
+                                        <p>{(artist.followers.total).toLocaleString('en-US')} followers</p>
+                                    </div>
+                                )
+                            })}
                         </div>
-
-                        <div className="card">
-                        <img className="img" src={logo} alt=""/>
-                            <h4>Tom</h4>
-                            <p>Followers</p>
-                            <h3>rating</h3>
-                        </div>
-
-                        <div className="card">
-                        <img className="img" src={logo} alt=""/>
-                            <h4>Tom</h4>
-                            <p>Followers</p>
-                            <h3>rating</h3>
-                        </div>
-
                     </div>
                 </div>
-            </div>
+                :
+                null
+            }
         </>
     )
 }
