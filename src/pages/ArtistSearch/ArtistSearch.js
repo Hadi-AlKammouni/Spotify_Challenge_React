fa5dac7420035e4c72b445dda06cd1572acb1da7
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-17 15:24:43 +0300

    Getting an artist's data upon searching

--- src/pages/ArtistSearch/ArtistSearch.js
+++ src/pages/ArtistSearch/ArtistSearch.js
@@ -5,7 +5,8 @@ import './styles.css'
 const ArtistSearch = () => {
 
     // Initialize Input State
-    const [artist, setArtist] = useState('');
+    const [artist, setArtist] = useState(null);
+    const [accessToken, setAccessToken] = useState("");
     
     // Function to separate the query parameter and its value from the response url
     const getReturnedParams = (hash) => {
@@ -19,24 +20,45 @@ const ArtistSearch = () => {
         }, {} )
         return paramsSplit
     }
+
+    // Calling searchForAnArtist function upon pressing the “Return” key inside the search box
+    const onSearch = (e) => {
+        e.preventDefault();
+        searchForAnArtist(artist)
+    }  
+    
+    // Getting an artist's data 
+    const searchForAnArtist = async (artist) => {
+        try {
+          const response = await fetch(`https://api.spotify.com/v1/search?q=artist%3A${artist}&type=artist`,{
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }
+            }).then (response => response.json())
+              .then(data=> console.log(data));
+        } catch (error) {
+          console.error(error);
+        }
+      }
     
     useEffect(() => {
         if(window.location.hash) {
             const object = getReturnedParams(window.location.hash)
-            console.log({ object })
+            setAccessToken(object.access_token)
         }
     },[])
 
     return (
         <div className="container">
             <div className='row'>
+            <form onSubmit={onSearch}>
                 <input 
                     type="text" 
                     placeholder="Search for an artist..." 
-                    onChange={(e) => {
-                        setArtist(e.target.value)
-                    }}
+                    onChange={(e) => setArtist(e.target.value)}
                 ></input>
+            </form>
             </div>
         </div>
     )
