5f4de21ec38f8e02a6acf4c0479f0ca405675978
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-18 11:32:11 +0300

    Fetching the albums for the selected artist

--- src/pages/Albums/Albums.js
+++ src/pages/Albums/Albums.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 import { useSpotify } from "../../context/spotify";
@@ -8,6 +9,30 @@ const Albums = () => {
     const artist_id = location.state.id
     const artist_name = location.state.name
     const {spotifyAccessToken} = useSpotify()
+
+    // Getting an artist's albums 
+    const getArtistAlbums = async (artist_id, spotifyAccessToken) => {
+        try {
+          const response = await fetch(`https://api.spotify.com/v1/artists/${artist_id}/albums`,{
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization": `Bearer ${spotifyAccessToken}`
+            }
+            })
+            .then (response => response.json())
+            .then(data => {
+                console.log(data)
+            });
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getArtistAlbums(artist_id,spotifyAccessToken)
+        console.log(artist_id)
+        console.log(spotifyAccessToken)
+    },[])
 
     return (
         <div>Here are the albums!</div>
