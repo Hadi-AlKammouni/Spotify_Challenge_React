d0af652b186150f7c68a7cb51ddea8c5ccc8a840
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-18 12:45:08 +0300

    Displaying album's info

--- src/pages/Albums/Albums.js
+++ src/pages/Albums/Albums.js
@@ -35,20 +35,25 @@ const Albums = () => {
 
     return (
         <div className="cards-body">
-            {console.log(albumsData)}
-                    <div className="cards-container">
-                        <div className="cards-row">
-                            {albumsData?.items.map((album, i) => {
-                                return (
-                                    <div className="card" key={i}>
-                                        <img className="img" src={album.images[1]?.url} alt="artist-image"/>
-                                        <h4>{album.name}</h4>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
+            <div className="cards-container">
+                <div className="cards-row">
+                    {albumsData?.items.map((album, i) => {
+                        return (
+                            <div className="card" key={i}>
+                                <img className="img" src={album.images[1]?.url} alt="artist-image"/>
+                                <h4>{album.name}</h4>
+                                {/* Displaying the artists of the album */}
+                                {album.artists.map((artist, i) => {
+                                    return (<p key={i}>{artist.name}</p>)
+                                })}
+                                <p>{album.release_date}</p>
+                                <p>{album.total_tracks} tracks</p>
+                            </div>
+                        )
+                    })}
                 </div>
+            </div>
+        </div>
     )
 }
 
